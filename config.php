a79d34c7bdcbdb2c0e4c8e11eb63da3c094db262
Gabriel CALLET <cgabrieldu13@gmail.com>
2025-02-01 15:21:15 +0100

    Ajout du fichier config.php pour la configuration

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+return [
+   'ia_used' => 'gemini', // ou 'gemini' si tu veux utiliser Gemini
+   'api_key_open_ai' => '',
+   'api_key_gemini' => 'AIzaSyAJBiPSQAAyKP3Zl2NvF1iEmht91e-ev0g',
+   'gemini_model' => 'gemini-1.5-flash-8b' // ou 'gemini-1.5-flash-latest'
+];
